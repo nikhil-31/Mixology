d89e68792582427a7156c67f60631c5bcdebd782
Nikhil B <nkl07ba@gmail.com>
2016-12-20 01:15:58 +0530

    Champagne fragment now inserts and loads data from db
    The Champagne fragment now loads data from the database

--- app/src/main/java/com/example/nik/mixology/Fragments/FragmentChampagne.java
+++ app/src/main/java/com/example/nik/mixology/Fragments/FragmentChampagne.java
@@ -1,9 +1,13 @@
 package com.example.nik.mixology.Fragments;
 
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -16,6 +20,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.nik.mixology.Adapters.DrinkCursorAdapter;
 import com.example.nik.mixology.Adapters.MainAdapter;
 import com.example.nik.mixology.Model.Cocktail;
 import com.example.nik.mixology.Network.VolleySingleton;
@@ -30,22 +35,24 @@ import java.util.ArrayList;
 
 import static com.example.nik.mixology.Network.CocktailURLs.COCKTAIL_SEARCH_URL_CHAMPAGNE_GLASS;
 import static com.example.nik.mixology.Network.CocktailURLs.COCKTAIL_SEARCH_URL_INGREDIENT_GIN;
+import static com.example.nik.mixology.data.DrinkProvider.ChampagneFlute.CONTENT_URI_CHAMPAGNE_FLUTE;
+import static com.example.nik.mixology.data.DrinkProvider.CocktailGlass.CONTENT_URI_COCKTAIL_GLASS;
 import static com.example.nik.mixology.utils.Utils.parseJSONResponse;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentChampagne extends Fragment {
+public class FragmentChampagne extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
 
     public String STATE_COCKTAIL = "state_cocktails";
     private String STATE_NULL = "null";
 
     private RecyclerView recyclerView;
-
+    private static final int CURSOR_LOADER_ID = 1;
     private ArrayList<Cocktail> mCocktailArrayList = new ArrayList<Cocktail>();
+    private DrinkCursorAdapter mDrinkAdapter;
 
-    private MainAdapter mAdapter;
     // Volley
     private RequestQueue mRequestQueue;
     private VolleySingleton mVolleySingleton;
@@ -53,6 +60,13 @@ public class FragmentChampagne extends Fragment {
 
     public FragmentChampagne() {
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getLoaderManager().initLoader(CURSOR_LOADER_ID, null, this);
+    }
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -70,12 +84,13 @@ public class FragmentChampagne extends Fragment {
         GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2);
         recyclerView.setLayoutManager(gridLayoutManager);
 
-        mAdapter = new MainAdapter(getActivity(), getActivity());
-        recyclerView.setAdapter(mAdapter);
+        mDrinkAdapter = new DrinkCursorAdapter(getActivity(), null, getActivity());
+        recyclerView.setAdapter(mDrinkAdapter);
+
 
         if (savedInstanceState != null) {
             mCocktailArrayList = savedInstanceState.getParcelableArrayList(STATE_COCKTAIL);
-            mAdapter.setCocktailList(mCocktailArrayList);
+
         } else {
             sendJsonRequest();
         }
@@ -90,6 +105,13 @@ public class FragmentChampagne extends Fragment {
         outState.putParcelableArrayList(STATE_COCKTAIL, mCocktailArrayList);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        getLoaderManager().restartLoader(CURSOR_LOADER_ID, null, this);
+
+    }
+
     private void sendJsonRequest() {
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET,
                 COCKTAIL_SEARCH_URL_CHAMPAGNE_GLASS,
@@ -101,8 +123,8 @@ public class FragmentChampagne extends Fragment {
                         try {
 
                             mCocktailArrayList.addAll(Utils.parseJSONResponse(response));
-                            mAdapter.setCocktailList(mCocktailArrayList);
 
+                            Utils.insertData(CONTENT_URI_CHAMPAGNE_FLUTE, mCocktailArrayList, getActivity());
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -118,4 +140,23 @@ public class FragmentChampagne extends Fragment {
     }
 
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(getActivity(),
+                CONTENT_URI_CHAMPAGNE_FLUTE,
+                null,
+                null,
+                null,
+                null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mDrinkAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mDrinkAdapter.swapCursor(null);
+    }
 }

ed8c4e2a45eefb10dfec386154930f259e852415
Nikhil B <nkl07ba@gmail.com>
2017-01-09 00:40:16 +0530

    Added sign out
    The user can signout using the menu

--- app/src/main/java/com/example/nik/mixology/Activities/ActivityMain.java
+++ app/src/main/java/com/example/nik/mixology/Activities/ActivityMain.java
@@ -1,5 +1,6 @@
 package com.example.nik.mixology.Activities;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -34,6 +35,9 @@ import com.example.nik.mixology.Fragments.FragmentNonAlcoholic;
 import com.example.nik.mixology.Model.Cocktail;
 import com.example.nik.mixology.R;
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ui.email.SignInActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
@@ -213,6 +217,19 @@ public class ActivityMain extends AppCompatActivity implements DrinkCursorAdapte
         if (id == R.id.action_settings) {
             return true;
         }
+        if(id == R.id.action_sign_out){
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        public void onComplete(@NonNull Task<Void> task) {
+                            // user is now signed out
+                            startActivity(new Intent(ActivityMain.this, SignInActivity.class));
+                            finish();
+                        }
+                    });
+        }
+
+
 
         return super.onOptionsItemSelected(item);
     }

a2a8ac8d8a7b0ca8aeb63910f9b8a149c2e2af47
Nikhil B <nkl07ba@gmail.com>
2017-01-12 00:22:06 +0530

    Search now uses an async task
    to meet the requirement in rubric the search fragment now uses an async task

--- app/src/main/java/com/example/nik/mixology/Fragments/FragmentSearch.java
+++ app/src/main/java/com/example/nik/mixology/Fragments/FragmentSearch.java
@@ -1,5 +1,9 @@
 package com.example.nik.mixology.Fragments;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -27,6 +31,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import javax.microedition.khronos.opengles.GL;
@@ -42,25 +56,10 @@ public class FragmentSearch extends Fragment {
     public FragmentSearch() {
     }
 
-    String Query;
-
-    // Volley
-    private RequestQueue mRequestQueue;
-    private VolleySingleton mVolleySingleton;
-
+    private String Query;
     private TextView mEmptyView;
-    private ArrayList<CocktailDetails> mCocktailDetails = new ArrayList<>();
-
     private RecyclerView mRecyclerView;
     private SearchAdapter mSearchAdapter;
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        mVolleySingleton = mVolleySingleton.getInstance();
-        mRequestQueue = mVolleySingleton.getmRequestQueue();
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -81,8 +80,29 @@ public class FragmentSearch extends Fragment {
         mSearchAdapter = new SearchAdapter(getActivity());
         mRecyclerView.setAdapter(mSearchAdapter);
 
-        sendJsonRequest();
+        if (isOnline()) {
+            OnTaskCompleted onTaskCompleted = new OnTaskCompleted() {
+                @Override
+                public void onMySearchTaskCompleted(ArrayList<CocktailDetails> cocktailDetailsArrayList) {
+                    mSearchAdapter.setCocktailList(cocktailDetailsArrayList);
+                }
+            };
+
+            MySearchTask mySearchTask = new MySearchTask(onTaskCompleted);
+            mySearchTask.execute(COCKTAIL_SEARCH_URL_BY_NAME + Query);
+        } else {
+            Toast.makeText(getActivity(), R.string.no_network_available, Toast.LENGTH_LONG).show();
+        }
+
         return rootView;
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        return isConnected;
     }
 
     @Override
@@ -92,30 +112,82 @@ public class FragmentSearch extends Fragment {
         mRecyclerView.setAdapter(mSearchAdapter);
     }
 
-    private void sendJsonRequest() {
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET,
-                COCKTAIL_SEARCH_URL_BY_NAME + Query,
-                null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
+    class MySearchTask extends AsyncTask<String, Void, ArrayList<CocktailDetails>> {
+        private final String LOG_TAG = MySearchTask.class.getSimpleName();
 
-                            mCocktailDetails = parseJSONResponse(response);
-                            mSearchAdapter.setCocktailList(mCocktailDetails);
+        private final OnTaskCompleted mListener;
 
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+        public MySearchTask(OnTaskCompleted listener) {
+            super();
+            mListener = listener;
+        }
+
+        @Override
+        protected ArrayList<CocktailDetails> doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String drinkJsonStr = null;
+
+            try {
+
+                String StringURL = params[0];
+                URL url = new URL(StringURL);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+
+                drinkJsonStr = buffer.toString();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
+                }
             }
-        });
-        mRequestQueue.add(request);
+            try {
+                JSONObject jsonObject = new JSONObject(drinkJsonStr);
+                return parseJSONResponse(jsonObject);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<CocktailDetails> cocktailDetailsArrayList) {
+            super.onPostExecute(cocktailDetailsArrayList);
+            mListener.onMySearchTaskCompleted(cocktailDetailsArrayList);
+
+        }
     }
+
 
     public ArrayList<CocktailDetails> parseJSONResponse(JSONObject response) throws JSONException {
 
